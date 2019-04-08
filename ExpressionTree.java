0fc7c68a0dec21e804bb8edf91884956f8f951df
Tyler Huang <thuang00@stuy.edu>
2019-04-08 14:38:20 -0400

    expresiion tree

--- ExpressionTree.java
+++ ExpressionTree.java
@@ -16,7 +16,12 @@ public class ExpressionTree{
   /* The sample tree would be: "3 2 10 * +"     */
   public String toStringPostfix(){
     /*you are to write this method*/
-    return "";
+    if (isValue()){
+      return "" + getValue();
+    }
+    else{
+      return getLeft().toStringPostfix() + " " + getRight().toStringPostfix() + " " + getOp();
+    }
   }
 
   /*return the expression as a prefix notation string without parenthesis*/
@@ -24,20 +29,42 @@ public class ExpressionTree{
 
   public String toStringPrefix(){
     /*you are to write this method*/
-    return "";
+    if (isValue()){
+      return "" + getValue();
+    }
+    else{
+      return getOp() + " " + getLeft().toStringPrefix() + " " + getRight().toStringPrefix();
+    }
   }
 
   /*return the value of the specified expression tree*/
   public double evaluate(){
     /*you are to write this method*/
-    return 0.0;
+    if (isValue()){
+      return getValue();
     }
+    else{
+      return apply(getOp(), getLeft().evaluate(),getRight().evaluate());
+    }
+  }
 
   /*use the correct operator on both a and b, and return that value*/
   private double apply(char op, double a, double b){
     /*you are to write this method*/
-    return 0.0;
+    if (op == '+'){
+      return a + b;
     }
+    if (op == '-'){
+      return a - b;
+    }
+    if (op == '*'){
+      return a * b;
+    }
+    if (op == '/'){
+      return a / b;
+    }
+    return 0.0;
+  }
 
   ////////////////////ONLY EDIT ABOVE THIS LINE////////////////////
 
